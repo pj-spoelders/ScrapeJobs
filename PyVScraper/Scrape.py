44e1aae0db9f72f17adc424b801564f0dbb40f46
Pieterjan Spoelders <pj.spoelders@gmail.com>
2019-09-03 12:31:43 +0200

    fixed issue of ajax calls to api after site opened with selenium / PhantomJS

--- PyVScraper/Scrape.py
+++ PyVScraper/Scrape.py
@@ -6,24 +6,36 @@ import time
 from bs4 import BeautifulSoup
 import re
 import html5lib
+from selenium import webdriver
 
 print("Hello")
 # https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
+# https://towardsdatascience.com/data-science-skills-web-scraping-javascript-using-python-97a29738353f
+# https://pythonspot.com/selenium-phantomjs/
+
 url = 'https://www.vdab.be/vindeenjob/vacatures?locatie=8630%20veurne&afstand=10&sort=standaard&jobdomein=JOBCAT10'
-response = requests.get(url)
-print(response)
+driver = webdriver.PhantomJS()
+# get web page
+driver.get(url)
+# execute script to scroll down the page
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+# sleep
+time.sleep(1)
+print("I slept")
+html = driver.page_source
+print(html)
 
-soup = BeautifulSoup(response.text)
-
-aResults = soup.find_all('a')
-strongFound = soup.find_all('strong')
-strongNumberFound = soup.find_all({"class": "vej-totaal-jobs-gevonden"})
-results = soup.find_all('div')
-divResults = soup.find_all('div', {"id":"results"})
-
-spans = soup.find_all('span')
-nrOfJobsSpan = soup.find('span', text=regexJobsGevonden)
-print(divResults)
+# response = requests.get(url)
+# print(response)
+#
+# soup = BeautifulSoup(response.text, 'html5lib')
+#
+# divResults = soup.find_all('div')
+#
+# regexJobsGevonden = re.compile("Jobs gevonden")
+# spans = soup.find_all('span')
+# nrOfJobsSpan = soup.find('span', text=regexJobsGevonden)
+# print(divResults)
 
 #print(spans)
 #print(nrOfJobsSpan)
