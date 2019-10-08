3608874f289c7c2d623455579957ca9d7298f5a5
Pieterjan Spoelders <pj.spoelders@gmail.com>
2019-10-08 21:28:53 +0200

    volledig werkende 1ste versie

--- PyVScraper/Scrape.py
+++ PyVScraper/Scrape.py
@@ -13,6 +13,7 @@ import html5lib
 from selenium import webdriver
 import dateparser
 
+
 def GetPageHtmlViaSelenium(driver, url):
     # get web page
     driver.get(url)
@@ -34,16 +35,20 @@ def GetInfoElementsFromPageSoup(soup, expectedResultsPerPage):
     return infoElements
 
 
+def GetPageSoupViaSelenium(driver, url):
+    pageHtml = GetPageHtmlViaSelenium(driver, url)
+    return BeautifulSoup(pageHtml)
+
+
 print("Start scraping")
 # https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
 # https://towardsdatascience.com/data-science-skills-web-scraping-javascript-using-python-97a29738353f
 # https://pythonspot.com/selenium-phantomjs/
 
-url = 'https://www.vdab.be/vindeenjob/vacatures?locatie=8630%20veurne&afstand=10&sort=standaard&jobdomein=JOBCAT10'
+url: str = 'https://www.vdab.be/vindeenjob/vacatures?locatie=8630%20veurne&afstand=10&sort=standaard&jobdomein=JOBCAT10'
 driver = webdriver.PhantomJS()
 
-page1Html = GetPageHtmlViaSelenium(driver, url)
-page1Soup = BeautifulSoup(page1Html)
+page1Soup = GetPageSoupViaSelenium(driver, url)
 
 
 def GetNrOfResultsFromPage():
@@ -56,7 +61,7 @@ def GetNrOfResultsFromPage():
 
 
 class JobInfo:
-    def __init__(self, title, href, company, location, jobtype,createdon, updatedon):
+    def __init__(self, title, href, company, location, jobtype, createdon, updatedon):
         self.Href = href
         self.Title = title
         self.Company = company
@@ -73,17 +78,19 @@ class JobInfo:
     CreatedOn: datetime.date
     UpdatedOn: datetime.date
 
+
 # https://realpython.com/python-json/#python-supports-json-natively
 class CustomJobInfoEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, JobInfo):
-            updatedOnStr :str
+            updatedOnStr: str
             if o.UpdatedOn is None:
                 updatedOnStr = ""
             else:
                 updatedOnStr = o.UpdatedOn.strftime("%d/%m/%Y")
-            return (o.Href,o.Title,o.Company,o.Location,o.JobType,o.CreatedOn.strftime("%d/%m/%Y"),updatedOnStr)
+            return (o.Href, o.Title, o.Company, o.Location, o.JobType, o.CreatedOn.strftime("%d/%m/%Y"), updatedOnStr)
         return super().default(o)
+
 
 def AddJobsFromInfoElements(infoElements: ResultSet):
     jobsList: List[JobInfo] = []
@@ -94,6 +101,7 @@ def AddJobsFromInfoElements(infoElements: ResultSet):
 
     return jobsList
 
+
 # expects '9 juli 2018' or similar
 def DutchDateStrToDate(dds: str):
     # just use smth like babel instead of messing about with locale
@@ -101,6 +109,7 @@ def DutchDateStrToDate(dds: str):
     # we're going to use dateparser https://github.com/scrapinghub/dateparser
     dt = dateparser.parse(dds, languages=['nl'])
     return dt
+
 
 def ParseInfo(info):
     infoTag: Tag = info.contents[1]
@@ -119,42 +128,34 @@ def ParseInfo(info):
         UpdatedOn = None
     Href = info.attrs["href"]
 
-    return JobInfo(Title, Href, Company, Location,JobType, CreatedOn, UpdatedOn)
-
+    return JobInfo(Title, Href, Company, Location, JobType, CreatedOn, UpdatedOn)
 
 
 nrOfResults = GetNrOfResultsFromPage()
 nrOfResultPerPage = 15
-nrOfOtherPagesToScrape = nrOfResults // nrOfResultPerPage - 1
-
+nrOfOtherPagesToScrape = nrOfResults // nrOfResultPerPage
+modulo = nrOfResults % nrOfResultPerPage
+if modulo > 0:
+    nrOfOtherPagesToScrape += 1
+nrOfOtherPagesToScrape -= 1
 page1InfoElements = GetInfoElementsFromPageSoup(page1Soup, nrOfResultPerPage)
 
 jobsList: List[JobInfo] = []
 
 jobsList += AddJobsFromInfoElements(page1InfoElements)
 
+# do the other pages as well
+# todo: possibly add a random interval if they're guarding against scraping
+i: int
+for i in range(0, nrOfOtherPagesToScrape):
+    pageXUrl = url + "&p=" + str(i + 2)
+    pageXSoup = GetPageSoupViaSelenium(driver, pageXUrl)
+    pageXInfoElements = GetInfoElementsFromPageSoup(pageXSoup, nrOfResultPerPage)
+    jobsList += AddJobsFromInfoElements(pageXInfoElements)
+
 with open("data_file.json", "w") as write_file:
-    json.dump(jobsList,fp=write_file,cls=CustomJobInfoEncoder)
-    #jsonStr = json.dumps(jobsList, cls=CustomJobInfoEncoder)
-
-
-# detailUrl = 'https://www.vdab.be/vindeenjob/vacatures/59880135/applicatiebeheerder-met-feeling-voor-testing-en-ervaring-in-een-labo-omgeving'
-# response = requests.get(detailUrl)
-# print(response.text)
-
-# print(response)
-#
-# soup = BeautifulSoup(response.text, 'html5lib')
-#
-# divResults = soup.find_all('div')
-#
-# regexJobsGevonden = re.compile("Jobs gevonden")
-# spans = soup.find_all('span')
-# nrOfJobsSpan = soup.find('span', text=regexJobsGevonden)
-# print(divResults)
-
-# print(spans)
-# print(nrOfJobsSpan)
+    json.dump(jobsList, fp=write_file, cls=CustomJobInfoEncoder)
+    # jsonStr = json.dumps(jobsList, cls=CustomJobInfoEncoder)
 
 print("end of program")
 # print(soup.findAll('a'))
