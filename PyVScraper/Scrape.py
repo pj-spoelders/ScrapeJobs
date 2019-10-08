97eeeec8f64d4107817d96a85d8463ee780a4458
Pieterjan Spoelders <pj.spoelders@gmail.com>
2019-10-08 14:37:17 +0200

    V1: scraping all needed info succesfully

--- PyVScraper/Scrape.py
+++ PyVScraper/Scrape.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 import random
 from typing import Optional, Any, List
 
@@ -17,15 +19,18 @@ def GetPageHtmlViaSelenium(driver, url):
     driver.execute_script(
         "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     # sleep
-    time.sleep(2)  # wait for content to load
+    time.sleep(10)  # wait for content to load, since the VDAB site is slow as molasses, give it a ton of time ..
     print("I slept")
     html = driver.page_source
     return html
 
-def GetInfoElementsFromPageSoup(soup):
+
+def GetInfoElementsFromPageSoup(soup, expectedResultsPerPage):
     infoElements = soup.find_all("a", {"class": "slat-link"})
+    assert len(infoElements) > 0 and len(infoElements) <= expectedResultsPerPage, "Something's wrong with the soup, expected more than 0 and no more than 15 elements"
     print("searched soup")
     return infoElements
+
 
 print("Start scraping")
 # https://towardsdatascience.com/how-to-web-scrape-with-python-in-4-minutes-bc49186a8460
@@ -40,43 +45,74 @@ page1Soup = BeautifulSoup(page1Html)
 
 
 def GetNrOfResultsFromPage():
-
-    nrOfResultsElement = page1Soup.find("strong", {"id": "vej-totaal-jobs-gevonden"})  # {"id": "vej-totaal-jobs-gevonden"})
+    nrOfResultsElement = page1Soup.find("strong",
+                                        {"id": "vej-totaal-jobs-gevonden"})  # {"id": "vej-totaal-jobs-gevonden"})
+    assert nrOfResultsElement is not None, "Page API call for content not executed yet."
     nrOfResultsStr = nrOfResultsElement.text
     nrOfResults = int(nrOfResultsStr)
     return nrOfResults
 
+
 class JobInfo:
+    def __init__(self, title, href, company, location, createdon, updatedon):
+        self.Href = href
+        self.Title = title
+        self.Company = company
+        self.Location = location
+        self.CreatedOn = createdon
+        self.UpdatedOn = updatedon
+
     Title: str
     Href: str
     Company: str
     Location: str
+    JobType: str
+    CreatedOn: datetime.date
+    UpdatedOn: datetime.date
 
-def AddJobsFromInfoElements(infoElements : ResultSet):
-    jobsList: List[str] = []
+
+def AddJobsFromInfoElements(infoElements: ResultSet):
+    jobsList: List[JobInfo] = []
 
     info: Tag
     for info in infoElements:
-        # Title = infoElements.contents
+        infoTag : Tag = info.contents[1]
+        Title = infoTag.find("div", {"class": "slat-title"}).text
+
+        locationTag : Tag = infoTag.find(class_="location")
+        locationTagStrongTags = locationTag.find_all("strong")
+        Company = locationTagStrongTags[0].text
+        Location =  locationTagStrongTags[1].text
+
+        jobTypeTag: Tag = infoTag.find(class_="job-type")
+        jobTypeSpanTags : ResultSet = jobTypeTag.find_all("span")
+        JobType = jobTypeSpanTags[0].text
+        CreatedOn = jobTypeSpanTags[1].text
+        if len(jobTypeSpanTags) == 3:
+            ChangedOn = jobTypeSpanTags[2].text
         Href = info.attrs["href"]
-        # jobsList.append(JobInfo(infoElements.attrs))
+
+
+        jobsList.append(JobInfo(infoElements.attrs))
 
     return jobsList
 
 
 nrOfResults = GetNrOfResultsFromPage()
 nrOfResultPerPage = 15
-nrOfOtherPagesToScrape = nrOfResults / nrOfResultPerPage - 1
+nrOfOtherPagesToScrape = nrOfResults // nrOfResultPerPage - 1
 
-page1InfoElements = GetInfoElementsFromPageSoup(page1Soup)
+page1InfoElements = GetInfoElementsFromPageSoup(page1Soup, nrOfResultPerPage)
 
 jobsList: List[JobInfo] = []
 
 jobsList += AddJobsFromInfoElements(page1InfoElements)
 
-#detailUrl = 'https://www.vdab.be/vindeenjob/vacatures/59880135/applicatiebeheerder-met-feeling-voor-testing-en-ervaring-in-een-labo-omgeving'
-#response = requests.get(detailUrl)
-#print(response.text)
+jsonStr = json.dumps(jobsList)
+
+# detailUrl = 'https://www.vdab.be/vindeenjob/vacatures/59880135/applicatiebeheerder-met-feeling-voor-testing-en-ervaring-in-een-labo-omgeving'
+# response = requests.get(detailUrl)
+# print(response.text)
 
 # print(response)
 #
